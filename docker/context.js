619996cd3edd9e09221ab4da35051f77a6329fe8
malain <alain.metge@zenasoft.com>
2017-12-10 22:00:55 +0100

    add docker and kbs template

---
+++ docker/context.js
@@ -0,0 +1,14 @@
+class Context {
+
+    prompts() {
+        return [];
+    }
+
+    init(options) {
+        this.options = options;
+        let self = this;
+        return new Promise((resolve, reject) => {
+            resolve("Dockerfile");
+        });
+    }
+}
---
+++ kubernetes/context.js
@@ -0,0 +1,19 @@
+class Context {
+
+    prompts() {
+        return [];
+    }
+
+    init(options) {
+        this.options = options;
+        options.fullName =  (options.serviceName + options.serviceVersion).replace(/[\.-]/g, '').toLowerCase();
+
+        if (!options.image) {
+            options.image = options.fullName;
+        }
+        let self = this;
+        return new Promise((resolve, reject) => {
+            resolve("Dockerfile");
+        });
+    }
+}
