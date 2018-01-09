3ac8ad3bda1022f928645f592304b34a9223f82f
METGE Alain <alain.metge@zenasoft.com>
2018-01-09 11:14:27 +0100

    update test script

--- generate.js
+++ generate.js
@@ -4,6 +4,14 @@ const Path = require('path');
 const unirest = require('unirest');
 const shell = require('shelljs');
 
+// Command to test
+let commandName = 'generate';
+
+// Initialize initial state
+let uri = 'http://localhost:8080';
+let template = "proxy";
+let state = { outputFolder: Path.join(shell.pwd().toString(), "generated"), address: uri + "/api/_servicedescription", template };
+
 class ContextBase {
     constructor(base) {
         this.baseFolder = base;
@@ -37,35 +45,16 @@ class ContextBase {
     }    
 }
 
-// Modify this for testing
-let uri = 'http://localhost:8080';             // Service address
-
-// Generate code from template
-let templateFolder = 'generate';
-let template = "proxy";
-
-let base = new ContextBase(templateFolder);
-
 try {
-    let state = { outputFolder: Path.join(shell.pwd().toString(), "generated"), address: uri + "/api/_servicedescription", template };
+    let base = new ContextBase(commandName);
     let ctx = base.createContextAsync("", state);
 
     ctx.exec()
-        .then((outputFile) => {
-            let template = fs.readFileSync(templateFolder + "/template.ejs", "utf8");
-            let txt = ejs.render(template, ctx);
-            outputFile = 'generatedFile.ts';
-
-            fs.writeFile(outputFile, txt, (err) => {
-                if (err) {
-                    console.log("Code generation error : " + err);
-                    return;
-                }
-                console.log("Code generated successfully in " + outputFile);
-            });
+        .then(() => {
+            console.log("Execution completed.");
         })
         .catch(err => {
-            console.log("Code generation error : " + err);
+            console.log("Error : " + err);
         });
 } catch (e) {
     console.log("Code generation error : " + e);
