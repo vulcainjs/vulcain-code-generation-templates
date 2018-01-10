dd0fed37ab03da106f7e7aa8f00e56772a4c2acb
METGE Alain <alain.metge@zenasoft.com>
2018-01-10 18:20:11 +0100

    add validation rules

--- init/index.js
+++ init/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 class Context {
 
     *prompts() {
-        yield { name: 'template', type: 'list', message: 'Select a template [--template]', validate: (v) => typeof v === "string" || "Template name is required", choices: this.context.getDirectories(this.context.commandFolder, 2) };
+        yield { name: 'template', type: 'list', message: 'Select a template [--template]', choices: this.context.getDirectories(this.context.commandFolder, 2) };
         yield { name: 'project', type: 'input', message: "Project name", validate: (s) => !!s };
         yield { name: 'outputFolder', type: 'input', message: "Generated output folder (without project name) [--outputFolder]", default: this.context.currentFolder };
 
