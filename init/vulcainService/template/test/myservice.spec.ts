e4b3cab06bd0e3695342e9994889c716c504257f
Alain Metge <malain@users.noreply.github.com>
2018-04-24 12:18:29 +0200

    Update myservice.spec.ts

--- init/vulcainService/template/test/myservice.spec.ts
+++ init/vulcainService/template/test/myservice.spec.ts
@@ -2,12 +2,13 @@ import { expect } from 'chai';
 import { TestContext } from 'vulcain-corejs';
 import { CustomerQueryHandler } from '../src/api/samples/handlers/queryHandler';
 import { CustomerActionHandler } from '../src/api/samples/handlers/actionHandler';
-import { Customer } from "../src/api/samples/models/models";
+// Ensures Customer schema is registered
+require("../src/api/samples/models/models");
 
 // Create a test context with an in-memory provider
 // and a test authorization policy (ignore authorization).
 // You can add a test user with .setUser()
-let context = new TestContext(Customer); // Initialize context with required model and service
+let context = new TestContext(); // Initialize context with required model and service
 
 describe("Default action handler", function () {
 
@@ -19,11 +20,11 @@ describe("Default action handler", function () {
     it("should create an entity", async function () {
         // Create an handler in a scoped (request) context
         let actionHandler = context.createHandler<CustomerActionHandler>(CustomerActionHandler);
-        let entity = { firstName: "elvis", lastName: "Presley" };
+        let entity = { firstName: "elvis", lastName: "Presley", id: "1" };
         await actionHandler.create(entity);
 
         let query = context.getService<CustomerQueryHandler>(CustomerQueryHandler.name); // Get a service within the scoped context
-        entity = await query.get("Presley");
+        entity = await query.get("1");
 
         expect(entity).to.be.not.null;
     });
